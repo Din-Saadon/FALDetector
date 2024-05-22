3e04d2cd4316688e213f4129f0451a129cdeeeba
Din-Saadon <104296281+Din-Saadon@users.noreply.github.com>
2024-05-22 12:55:17 +0300

    Update preprocess_data.py

--- preprocess_data.py
+++ preprocess_data.py
@@ -2,23 +2,26 @@ import argparse
 import os
 
 def main():
-    parser = argparse.ArgumentParser(description="Parse image directory paths and flow directories.")
+    parser = argparse.ArgumentParser(
+        description="Parse image directory paths and flow directories.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
 
     # Argument for modified image directory path
     parser.add_argument('--mod', type=str, default='../sources/images/modified',
-                        help='Path to the directory containing modified images. Default is ../sources/images/modified.')
+                        help='Path to the directory containing modified images.')
 
     # Argument for reference image directory path
     parser.add_argument('--ref', type=str, default='../sources/images/reference',
-                        help='Path to the directory containing reference images. Default is ../sources/images/reference.')
+                        help='Path to the directory containing reference images.')
 
     # Argument for forward flow directory path
     parser.add_argument('--flow_fwd', type=str, default='../sources/flows/fwd',
-                        help='Path to the directory containing forward flow files. Default is ../sources/flows/fwd.')
+                        help='Path to the directory where forward flow files will be saved.')
 
     # Argument for backward flow directory path
     parser.add_argument('--flow_bwd', type=str, default='../sources/flows/bwd',
-                        help='Path to the directory containing backward flow files. Default is ../sources/flows/bwd.')
+                        help='Path to the directory where backward flow files will be saved.')
 
     # Parse the arguments
     args = parser.parse_args()
