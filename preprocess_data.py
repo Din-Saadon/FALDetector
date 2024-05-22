a3d8471557d21227161f7cb48a915a613dfc07bb
Din-Saadon <104296281+Din-Saadon@users.noreply.github.com>
2024-05-22 12:51:52 +0300

    Update preprocess_data.py

--- preprocess_data.py
+++ preprocess_data.py
@@ -29,11 +29,13 @@ def main():
     flow_fwd_path = args.flow_fwd
     flow_bwd_path = args.flow_bwd
 
-    print(f"python modules/SpyNet/pred.py --model sintel-final --one {mod_path} --two {ref_path} --out {flow_fwd_path}")
-  
+    # Ensure the output directories exist
+    os.makedirs(flow_fwd_path, exist_ok=True)
+    os.makedirs(flow_bwd_path, exist_ok=True)
+
     # pred SpyNet
-    #os.system(f"python modules/SpyNet/pred.py --model sintel-final --one {mod_path} --two {ref_path} --out {flow_fwd_path}")
-    #os.system(f"python modules/SpyNet/pred.py --model sintel-final --one {ref_path} --two {mod_path} --out {flow_bwd_path}")
+    os.system(f"python modules/SpyNet/pred.py --model sintel-final --one {mod_path} --two {ref_path} --out {flow_fwd_path}")
+    os.system(f"python modules/SpyNet/pred.py --model sintel-final --one {ref_path} --two {mod_path} --out {flow_bwd_path}")
 
 if __name__ == "__main__":
     main()
