4ee10d1e8d11ad7efbf8e677274aeaf1acc0a8a9
K-LightYagami <din.saadon1@gmail.com>
2024-05-28 23:24:41 +0300

    .

--- .ipynb_checkpoints/preprocess_data-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-#pred SpyNet
-os.system("python modules/SpyNet/pred.py --model sintel-final --one ../sources/images/modified --two ../sources/images/reference --out ../sources/flows/fwd")
-os.system("python modules/SpyNet/pred.py --model sintel-final --one ../sources/images/reference --two ../sources/images/modified --out ../sources/flows/bwd ")
-
-
-
--- .ipynb_checkpoints/run_demo-checkpoint.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python pred.py --input_path ../sources/images/modified/openimage_0995.png --dest_folder ../res/ --model_path ../checkpoints/DRN/local.pth
